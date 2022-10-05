61cbf0f1bee5bf99fe59f8f981de6151de056898
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-10-04 21:56:37 -0400

    first commit, added recycler view, textview, image view, and grid layout manager

--- app/src/main/java/edu/temple/assignment4/ImageActivity.kt
+++ app/src/main/java/edu/temple/assignment4/ImageActivity.kt
@@ -2,10 +2,29 @@ package edu.temple.assignment4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity()
+{
+    //variables with the assigned view type
+    lateinit var placeName: TextView
+    lateinit var placeImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycleView)
+        placeName = findViewById(R.id._placeName)
+        placeImage = findViewById(R.id._placeImage)
+
+        //grid layout manager
+        recyclerView.layoutManager = GridLayoutManager(this, 4)
+
+
+
     }
 }
